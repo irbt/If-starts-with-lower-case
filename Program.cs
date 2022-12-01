847493793b94f13c2e9ddef7d2bf796dfc589f85
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-01 15:04:39 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Given a string, write a method that checks if each word in the string starts with lower case and if so, removes this letter from the string.
+
+namespace Program{
+    class IfStartsWithLowerCaseTASK{
+
+        public static string IfStartsWithLowerCase (string){
+            
+        }
+        public static void Main(){
+
+        }
+    }
+}
